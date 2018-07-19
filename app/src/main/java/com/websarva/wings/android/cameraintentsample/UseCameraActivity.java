f38f65c95d25002f5f027b0c9fa4bf2b8e100523
kishimotsu72 <kishimotsu72@gmail.com>
2018-07-19 09:36:30 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/cameraintentsample/UseCameraActivity.java
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.cameraintentsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UseCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_use_camera);
+    }
+}
