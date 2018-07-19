28bc6a2ec0696ec5af2859b6b03892cc8bfd80f4
kishimotsu72 <kishimotsu72@gmail.com>
2018-07-19 10:32:08 +0900

    Bitmap

--- app/src/main/java/com/websarva/wings/android/cameraintentsample/UseCameraActivity.java
+++ app/src/main/java/com/websarva/wings/android/cameraintentsample/UseCameraActivity.java
@@ -1,7 +1,21 @@
 package com.websarva.wings.android.cameraintentsample;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UseCameraActivity extends AppCompatActivity {
 
@@ -10,4 +24,23 @@ public class UseCameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_use_camera);
     }
-}
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == 200 && resultCode == RESULT_OK) {
+
+            Bitmap bitmap = data.getParcelableArrayExtra("data");
+
+            ImageView ivCamera = findViewById(R.id.ivCamera);
+
+            ivCamera.setImageBitmap(bitmap);
+        }
+    }
+
+    public void onCameraImageClick(View view) {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivityForResult(intent, 200);
+    }
+    }
